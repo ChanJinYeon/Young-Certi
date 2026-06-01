a5545a8fcf73a362fe02ff59a98cb45c2247bdfb
Adansonia <adansonia@Adansoniaui-Macmini.local>
2026-06-01 13:09:16 +0900

    edit backend path

--- infra/envs/dev-foundation/backend.tf
+++ infra/envs/dev-foundation/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "young-certi-tfstate-study"
-    key          = "dev-foundation/terraform.tfstate"
+    key          = "dev/foundation/terraform.tfstate"
     region       = "ap-northeast-2"
     encrypt      = true
     use_lockfile = true
